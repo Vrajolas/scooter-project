9c04ff55c595759b2e0f4b2a3ff3874c11f6551d
Vrajolas <vrajeshrajpro11@gmail.com>
2023-02-14 10:10:16 +0000

    Done a couple of tests for th user and scooter classes

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,23 +1,26 @@
 const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 
-//typeof scooter === object
-describe('scooter object', () => {
-  test('does something', () => {
-    // edit this to be a real test!
-    expect(false).toEqual(true);
-  }
-)
-})
-
 //Method tests
 describe('scooter methods', () => {
-  // tests here!
+  let scooter;
+  beforeEach(() => {
+    scooter = new Scooter("station", "user", 50, false)
+  })
 
   //rent method
-
+  test('rent method should update station to null and user to station1 if scooter is charged and not broken', () => {
+    scooter.rent();
+    expect(scooter.station).toBe(null);
+    expect(scooter.user).toBe('station');
+  });
   //dock method
-
+  test('rent method should throw an error if scooter is not charged', () => {
+    scooter.charge = 10;
+    expect(() => {
+      scooter.rent();
+    }).toThrowError('scooter needs to charge');
+  });
   //requestRepair method
 
   //charge method
