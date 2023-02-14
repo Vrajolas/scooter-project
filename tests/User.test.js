badb0465ec5386d7930b9a5d5b3e4fde57d8de92
Vrajolas <vrajeshrajpro11@gmail.com>
2023-02-14 10:09:23 +0000

    Done a couple of tests for the scooter an user classes

--- tests/User.test.js
+++ tests/User.test.js
@@ -1,13 +1,28 @@
 const User = require('../src/User')
 
 // User tests here
+describe("User", () => {
+    let user;
+    beforeEach(() => {
+        user = new User("vrajesh", "password123", "19")
+    })
 
 // test username
+    it('creates a new user with the given properties', () => {
+        expect(user.username).toBe('john');
+        expect(user.age).toBe(30);
+        expect(user.loggedIn).toBe(false);
+    });
 
 // test password
-
+    it('logs in the user when correct password is provided', () => {
+        user.login('password');
+        expect(user.loggedIn).toBe(true);
+    });
 // test age
 
 // test login
 
 // test logout
+
+})
